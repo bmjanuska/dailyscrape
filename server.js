01e1b779f90d31d9d34c3f11e58857ad71c78a3b
Bernadette Januska <bmjanuska@gmail.com>
2018-09-09 11:40:40 -0400

    API routes set up.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//DEPENDANCIES
+//=============================================
 require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -5,11 +7,17 @@ var exphbs = require("express-handlebars");
 // var mongoose = require("mongoose");
 // var cheerio = require("cherrio");
 // var request = require("request");
+var mongojs = require("mongojs");
 
+//INITIALIZE EXPRESS
+//=============================================
+var app = express();
+
+//DB CONFIG
+//=============================================
 var db = require("./models");
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+// var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,15 +45,10 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
 
+//=====================================================================
 module.exports = app;
